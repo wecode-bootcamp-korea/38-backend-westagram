aaed4606a20fe807aef8812c8c9a3876031cf1ef
iflov <wjdgusxo99@gmail.com>
2022-10-06 20:38:27 +0900

    2번과제 진행 하였습니다 (회원가입)

---
+++ teamA/leehyeontae/app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const morgan = require('morgan');
+
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const { DataSource } = require('typeorm');
+
+const PORT = process.env.PORT;
+const appDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
+})
+
+appDataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+      console.error("Error during Data Source initialization", err)
+      appDataSource.destroy()
+    })
+
+
+app.post('/signup' , async (req,res) => {
+      const {name,email,profile_image,password} = req.body;
+
+      await appDataSource.query(
+        `INSERT INTO users(
+          name,
+          email,
+          profile_image,
+          password
+          ) VALUES (?,?,?,?)
+         ` ,
+         [name,email,profile_image,password]
+      );
+      res.status(201).json({message : 'user created'});
+})
+
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+}
+start();
