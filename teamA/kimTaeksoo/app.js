bf1a874e39ada276d350413f7dd1826dd3c52c79
Robinkim93 <kal6529@gmail.com>
2022-10-06 16:47:03 +0900

    initial setting ver2.

--- teamA/kimTaeksoo/app.js
+++ teamA/kimTaeksoo/app.js
@@ -1,24 +1,33 @@
-// express 및 라이브러리 import
+const dotenv = require("dotenv").config();
+
 const http = require("http");
+
 const express = require("express");
-const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
 
-// express를 app에 담아 method 사용이 가능해진다.
+const { DataSource } = require("typeorm");
 const app = express();
-// dotenv를 사용할 수 있게 해준다.
-dotenv.config();
-// 미들웨어로써 다양한 라이브러리를 use를 통해 등록할 수 있다.
-// cors로 다른 도메인간 엄격한 통신을 완화시켜준다.
-// morgan을 통해 log관리를 해줄 수 있다.
-app.use(cors());
-app.use(morgan("dev"));
-// app을 http통신을 이용한 서버로 만들어준다.
 const server = http.createServer(app);
 
 const PORT = process.env.PORT;
+const myDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
 
+myDataSource.initialize().then(() => {
+  console.log("data source has been init");
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+
+// health check
 app.get("/ping", (req, res, next) => {
   res.json({ message: "success" });
 });
