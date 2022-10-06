f42396e6f6a6e0478a92607eac5c39cd98f33f0c
이찬우 <scflcweric1201@gmail.com>
2022-10-06 16:20:07 +0900

    Assignment 1 | Westagram Backend Project - Express 초기 환경세팅

---
+++ teamA/leeChanWoo/app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
+const { DataSource } = require('typeorm');
+
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.get('/ping', function (req, res, next) {
+     res.json({message: 'pong'});
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, function () {
+     console.log('server listening on port 3000');
+});
