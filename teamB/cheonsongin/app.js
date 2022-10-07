a13da8ceebb901479aa36a6e84fdfdf58fbe7405
csongin <cheonsong37@gmail.com>
2022-10-07 14:08:33 +0900

    [add] 게시글 등록, 전체 게시글 조회하기 작업

--- teamB/cheonsongin/app.js
+++ teamB/cheonsongin/app.js
@@ -33,6 +33,40 @@ app.get('/ping', (req, res) => {
   res.json({ message : 'pong' });
 });
 
+// 게시물 등록
+app.post('/posts/upload', async (req, res, next) => {
+  const { title, post_image, content, user_id  } = req.body;
+  
+  await appDataSource.query(
+    `INSERT INTO posts(
+      title,
+      post_image,
+      content,
+      user_id
+    ) VALUES (?, ?, ?, ?);
+    `,
+    [ title, post_image, content, user_id ]
+  );
+
+  res.status(201).json({ message : 'postCreated' });
+});
+
+// 전체 게시물 조회
+app.get('/posts/get', async (req, res) => {  
+  await appDataSource.query(
+    `SELECT
+      p.user_id AS userId,
+      u.profile_image AS userProfileImage,
+      p.id AS postingId,
+      p.post_image AS postingImageUrl,
+      p.content AS postingContent
+      FROM users u, posts p`
+    ,(err, rows) => {
+      res.status(200).json({ data : rows });
+    }
+  );
+});
+
 const server = http.createServer(app);
 const PORT = process.env.PORT;
 
