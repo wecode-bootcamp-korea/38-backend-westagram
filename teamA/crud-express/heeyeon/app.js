02d134e0c62a0d3efcd3870fd7f713aea6982e5c
heeyeon <k.heeyeon22@gmail.com>
2022-10-07 15:09:55 +0900

    Assignment 2: Express API - 유저 회원가입 하기

---
+++ teamA/crud-express/heeyeon/app.js
@@ -0,0 +1,71 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const http = require('http');
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { DataSource } = require('typeorm');
+
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPOPRM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+.then(() => {
+    console.log('Data Source has been initialized!')
+});
+
+const app = express()
+
+app.use(express.json());
+app.use(morgan('tiny'));
+
+app.get('/ping', cors(), function (req, res, next) {
+    res.status(200).json({ message : 'pong '})
+});
+
+app.post('/users/signup', async (req, res, next) => {
+    const {id, name, email, profile_image, password} = req.body
+
+    await myDataSource.query(
+        `INSERT INTO users(
+            id,
+            name,
+            email,
+            profile_image,
+            password
+        ) VALUES (?, ?, ?, ?, ?);
+        `,
+        [id, name, email, profile_image, password]
+    );
+
+    res.status(201).json({ message : "userCreated" })
+
+})
+
+
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
+
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// const start = async() => {
+//     server.listen(PORT, () => console.log(`server is listening on ${PORT}`))
+// }
+
+start()
