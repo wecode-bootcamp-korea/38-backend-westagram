6453a81592433ed6aab4deb41b2952f0da74715b
김성훈 <m1_air@M1air.local>
2022-10-07 20:36:15 +0900

    Assignment3 게시글 등록하기

---
+++ teamB/kimSungHoon/db/migrations/20221007111253_create_likes_table.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+CREATE TABLE likes (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT likes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT likes_post_id_fkey FOREIGN KEY (post_id) REFERENCES posts(id)
+);
+
+-- migrate:down
+DROP TABLE likes;
