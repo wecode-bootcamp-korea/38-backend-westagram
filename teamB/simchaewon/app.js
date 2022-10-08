c69bb734ebee7b278d1f992e8a1eedd00633dde1
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-08 18:25:13 +0900

    [teamB:심채원]Assignment 1 - initial setting

--- teamB/simchaewon/app.js
+++ teamB/simchaewon/app.js
@@ -4,10 +4,32 @@ const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
 
+const {DataSource}=require("typeorm");
+
+const myDataSource = new DataSource(
+    {
+    type:process.env.TYPEORM_CONNECTION,
+    host:process.env.TYPEORM_HOST,
+    port:process.env.TYPEORM_PORT,
+    username:process.env.TYPEORM_USERNAME,
+    password:process.env.TYPEORM_PASSWORD,
+    database:process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+    .then(()=>{
+        console.log("Data Source has been initialized!");
+    }) 
+    .catch((err)=>{
+            console.error("Error during Data Sources initialization");
+            myDataSource.destroy();
+    });
+
 const app = express();
 app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(morgan("dev"));
+
 
 app.get("/ping",(req, res)=>{
     res.status(200).json({"message":"pong"});
@@ -24,6 +46,5 @@ const start = async()=>{
     } catch(err){
         console.error(err);
     }
-}
-
-
+};
+    start();
