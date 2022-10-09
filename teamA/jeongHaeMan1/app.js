6118fd5a8294061cef426a5ec5fd763e442bbac8
jeonghaeman <haemong_2@naver.com>
2022-10-09 21:12:17 +0900

    crud

--- teamA/jeongHaeMan1/app.js
+++ teamA/jeongHaeMan1/app.js
@@ -4,12 +4,7 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const routes = require('../routes');
-
-app.get("/ping", (req, res) => {
-    res.status(200).json({ message : "pong"})
-});
-
+const routes = require('./routes');
 
 const app = express();
 
@@ -18,11 +13,16 @@ app.use(cors());
 app.use(morgan(''));
 app.use(routes);
 
+app.get("/ping", (req, res) => {
+    res.status(200).json({ message : "pong"})
+});
+
 const PORT = process.env.PORT;
 const server = http.createServer(app);
 
 const start = async () => {
     try {
+        server.listen()
 
     } catch (err) {
         console.error(err)
