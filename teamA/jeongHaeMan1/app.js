4ff37e5e111c738a8e1e9cababa1473dc1580d0c
jeonghaeman <haemong_2@naver.com>
2022-10-09 21:09:16 +0900

    crud

---
+++ teamA/jeongHaeMan1/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('../routes');
+
+app.get("/ping", (req, res) => {
+    res.status(200).json({ message : "pong"})
+});
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan(''));
+app.use(routes);
+
+const PORT = process.env.PORT;
+const server = http.createServer(app);
+
+const start = async () => {
+    try {
+
+    } catch (err) {
+        console.error(err)
+    }
+}
