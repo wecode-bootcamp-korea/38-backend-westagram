ec82cc1b2398dd02554b75d3ec1f62070ec3b26e
jeonghaeman <haemong_2@naver.com>
2022-10-09 14:22:26 +0900

    inital-setting

---
+++ teamA/jeongHaeMan1/app.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const { DataSource } = require('typeorm');
+
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    database: process.env.TYPEORM_DATABASE
+})
+
+myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initializtion", err)
+        myDataSource.destroy()
+    })
+
+app = express();
+
+app.use(express.json());
+app.use(cors());
+app.morgan(morgan(''));
+
+app.get("/ping", (req, rse) => {
+    res.json({message : "pong"})
+});
+
+const PORT = process.env.PORT;
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`server is listening on ${PORT}`)
+        })
+    } catch (err) {
+        console.error(err)
+    }
+};
+
+start();
