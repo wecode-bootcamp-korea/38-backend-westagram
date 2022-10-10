f365bee9fc0786089d1a6def799bdc50ea159b59
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-10 15:26:25 +0900

    for layered => initial setting

---
+++ teamB/simchaewon/db/migrations/20221010055814_create_posts_table.sql
@@ -0,0 +1,15 @@
+-- migrate:up
+CREATE TABLE posts (
+    id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(200) NOT NULL,
+    content VARCHAR(1000) NOT NULL,
+    user_id INT NOT NULL,
+    post_image VARCHAR(200) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- migrate:down
+DROP TABLE posts;
