44ad89957147f0c046319dc970d162037712fd24
김성훈 <m1_air@M1air.local>
2022-10-10 15:00:29 +0900

    Assignment5 유저의 게시글 조회하기

--- teamB/kimSungHoon/app.js
+++ teamB/kimSungHoon/app.js
@@ -84,7 +84,36 @@ app.get('/posts-with-users', async (req, res) => {
         (err, data) => {
         res.status(200).json({"data" : data});
 	});
-})    
+})
+
+app.get('/posts-from-user/:userid', async (req,res)=>{
+
+    const userid  = parseInt(req.params.userid);
+
+    await Promise.all([
+        appDataSource.query(
+            `SELECT
+                posts.id AS postingId,
+                posts.title AS postingTitle,
+                posts.content AS postingContent
+            FROM posts
+            WHERE user_id= ${userid};
+            `, 
+        ),
+        appDataSource.query(
+            `SELECT
+                users.id AS userId,
+                users.profile_image AS userProfileImage
+            FROM users
+            WHERE id = ${userid};
+            `
+        )])
+        .then(([postingsResult, userResult])=>{
+            userResult[0]['postings'] = postingsResult;
+            res.status(200).json({ "data" : userResult[0] });
+        })
+    
+})
 
 const server = http.createServer(app);
 
