218ebb8e5e1f57685057089ea063045e02c4d0ce
jeonghaeman <haemong_2@naver.com>
2022-10-11 16:26:10 +0900

    sign-up

--- teamA/jeongHaeMan1/services/userService.js
+++ teamA/jeongHaeMan1/services/userService.js
@@ -3,15 +3,6 @@
 const userDao = require('../models/userDao')
 
 const signUp = async (name, email, password, profileImage) => {
-    //password validation using REGEX
-    // const pwValidation = new RegExp(
-    //     '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})'
-    // );
-    // if (!pwValidation.test(password)) {
-    //     const err = new Error('PASSWORD_IS_NOT_VALID');
-    //     err.statusCode = 400;
-    //     throw err;
-    // }
     const createUser = await userDao.createUser(
         name,
         email,
