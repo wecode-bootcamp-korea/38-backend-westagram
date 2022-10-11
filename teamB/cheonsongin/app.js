c3a646f5f654af5854bf399e18c0fde94de376a1
csongin <cheonsong37@gmail.com>
2022-10-11 13:28:59 +0900

    [modify] sql 쿼리문 포맷팅

--- teamB/cheonsongin/app.js
+++ teamB/cheonsongin/app.js
@@ -133,14 +133,10 @@ app.post('/likes', async(req, res) => {
   const { user_id, post_id } = req.body;
 
   await appDataSource.query(
-    `INSERT INTO likes
-    (
+    `INSERT INTO likes(
       user_id,
       post_id
-    )
-    VALUES
-    (?, ?);
-    `,
+      ) VALUES (?, ?);`,
     [ user_id, post_id ]
   );
   res.status(201).json({ message : 'likeCreated' });
