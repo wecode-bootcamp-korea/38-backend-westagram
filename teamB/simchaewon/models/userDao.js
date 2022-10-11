12d5800cfba88f75d8e5aa39dd4c4d38af8e0a15
tlacodnjs667 <tlacodnjs667@gmail.com>
2022-10-11 14:13:55 +0900

    Assignment8 completed

--- teamB/simchaewon/models/userDao.js
+++ teamB/simchaewon/models/userDao.js
@@ -1,81 +1,79 @@
-const { DataSource } = require('typeorm');
+const { DataSource } = require("typeorm");
 
 const westa_DB = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-})
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
 
-westa_DB.initialize()
+westa_DB
+  .initialize()
   .then(() => {
     console.log("Data Source has been initialized!");
   })
   .catch((err) => {
     console.error("Error occurred during Data Source initialization", err);
-	  westa_DB.destroy();
+    westa_DB.destroy();
   });
 
-
-const createUser = async ( name, email, password, profileImage ) => {
-	try {
-		await westa_DB.query(
-			`INSERT INTO users(
+const createUser = async (name, email, password, profileImage) => {
+  try {
+    await westa_DB.query(
+      `INSERT INTO users(
 				name,
 				email,
 				password,
 				profile_image
 				) VALUES (?, ?, ?, ?);
 				`,
-				[ name, email, password, profileImage ]
-				);
-	} 
-    
-    catch (err) {
-		const error = new Error('INVALID_DATA_INPUT');
-		error.statusCode = 500;
-		throw error;
-	}
+      [name, email, password, profileImage]
+    );
+  } catch (err) {
+    const error = new Error("INVALID_DATA_INPUT");
+    error.statusCode = 500;
+    throw error;
+  }
 };
 
-const getUserPosts = async(userName)=>{
-	try{
-		const userId = await westa_DB.query("SELECT id FROM users WHERE name=?;", [userName]);
-		const userID = await userId[0].id;
-		const user_posts=await westa_DB.query(
-			`SELECT users.id AS userId, users.profile_image AS userProfileImage, posts.id AS postingId, posts.post_image AS postingImageUrl, posts.content FROM users LEFT JOIN posts ON users.id=posts.user_id WHERE users.id=${userID} ORDER BY posts.id;`
-		);
-		const postingArr = [];
-		
-		await user_posts.forEach((el)=>{
-			postingArr.push({
-				"postingId"        : el.postingId,
-				"postingImageUrl"  : el.postingImageUrl,
-				"postingContent"   : el.content
-			});
-		});
-		
-		
-		const result = await {	
-			"userId" : user_posts[0].userId,
-			"userProfileImage" : user_posts[0].userProfileImage,
-			"postings" : postingArr 
-		};
-		
-		// await console.log(result);
-	
-		return await result;
-	}
+const getUserPosts = async (userName) => {
+  try {
+    const userId = await westa_DB.query(`SELECT id FROM users WHERE name=?;`, [
+      userName,
+    ]);
+    const userID = await userId[0].id;
+    const user_posts = await westa_DB.query(
+      `SELECT users.id AS userId, users.profile_image AS userProfileImage, posts.id AS postingId, posts.post_image AS postingImageUrl, posts.content FROM users LEFT JOIN posts ON users.id=posts.user_id WHERE users.id=${userID} ORDER BY posts.id;`
+    );
+    const postingArr = [];
 
-	catch(err){
-		const error =new Error('INVALID_DATA_INPUT');
-		error.statusCode = 500;
-		throw error;
-	}
-}
+    await user_posts.forEach((el) => {
+      postingArr.push({
+        postingId: el.postingId,
+        postingImageUrl: el.postingImageUrl,
+        postingContent: el.content,
+      });
+    });
+
+    const result = await {
+      userId: user_posts[0].userId,
+      userProfileImage: user_posts[0].userProfileImage,
+      postings: postingArr,
+    };
+
+    // await console.log(result);
+
+    return await result;
+  } catch (err) {
+    const error = new Error("INVALID_DATA_INPUT");
+    error.statusCode = 500;
+    throw error;
+  }
+};
 
 module.exports = {
-  createUser, getUserPosts
-}
+  createUser,
+  getUserPosts,
+};
