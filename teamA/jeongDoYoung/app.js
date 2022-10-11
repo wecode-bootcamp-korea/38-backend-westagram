75a0fdbb80ce9995fd1c4cc9355fa6273d92ff25
정도영 <momtop11@gmail.com>
2022-10-11 17:28:40 +0900

    Layered Pattern 제거

--- teamA/jeongDoYoung/app.js
+++ teamA/jeongDoYoung/app.js
@@ -5,53 +5,53 @@ const http = require("http");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { DataSource } = require("typeorm");
+const exp = require("constants");
 //custom package
 const app = express();
-const routes = require("./routes");
-const routes = require("./routes");
-const { DataSource } = require('typeorm');
+const server = http.createServer(app);
+const PORT = process.env.PORT;
+
 
 const myDataSource = new DataSource({
-  type: process.env.TYPOEORM_CONNECTION,
-  host: process.env.TYPOEORM_HOST,
-  port: process.env.TYPOEORM_PORT,
-  username: process.env.TYPOEORM_USERNAME,
-  password: process.env.TYPOEORM_PASSWORD,
-  database: process.env.TYPOEORM_DATABASE
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
 });
-myDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
-  }); 
+
+myDataSource.initialize().then(() => {
+  console.log("data source has been initialized");
+});
+
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
-//[GET] 통신 heath check code
-app.get('/ping', async (req, res) => {
-  res.json({ "message" : "pong" });
+app.use(morgan("dev"));
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
 });
 
-//[POST] user sign up code
 app.post('/signup', async (req, res) => {
-  const { name, email, profile_image, password } = req.body;
-
-  await myDataSource.query(
-    `
+  const {name,email,profile_image,password} = req.body
+  await myDataSource.query(`
     INSERT INTO users (
       name,
       email,
       profile_image,
       password
-    ) VALUES ( ?, ?, ?, ? );
-    `,
+    ) VALUES ( ?, ?, ?, ? );`,
     [ name, email, profile_image, password ]
   );
   res.status(201).json({ "message" : "userCreated" });
 });
 
-const server = http.createServer(app);
-const PORT = process.env.PORT;
-const start = async () => {
-  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+const start = () => {
+  server.listen(PORT, () => {
+    console.log(`connected server ${PORT}`);
+  });
 };
+
 start();
