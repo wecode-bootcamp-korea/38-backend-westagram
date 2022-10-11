8bf9a393e875f1ba23aa3bb93d6e7434322ba376
jeonghaeman <haemong_2@naver.com>
2022-10-11 16:41:27 +0900

    initial-setting

--- teamA/jeongHaeMan1/db/migrations/20221009052902_create_likes_table.sql
+++ teamA/jeongHaeMan1/db/migrations/20221009052902_create_likes_table.sql
@@ -3,7 +3,8 @@ CREATE TABLE likes (
     id INT NOT NULL PRIMARY KEY,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
-    CONSTRAINT likes_user_id_fkey, likes_post_id_fkey FOREIGN KEY (user_id, post_id) REFERENCES users(id), posts(id) ON DELETE CASCADE
+    CONSTRAINT likes_user_id_fkey FOREIGN KEY user_id REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT likes_post_id_fkey FOREIGN KEY post_id REFERENCES posts(id) ON DELETE CASCADE
 )
 
 -- migrate:down
