c023c8515e4c0f41e4722c7a2210699351c4fb45
SHIN INHUCK <sih9492@gmail.com>
2022-10-11 16:30:38 +0900

    [FIX] sign-up

--- teamB/shinInHuck/myapp/app.js
+++ teamB/shinInHuck/myapp/app.js
@@ -1,50 +1,30 @@
-require('dotenv').config();
-
-const http = require('http');
+const http = require("http");
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const dotenv = require("dotenv").config();
+const routes = require("./routes");
 
-const { DataSource } = require('typeorm');
+const app = express();
 
-const myDataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    database: process.env.TYPEORM_DATABASE
-})
-
-myDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initializtion", err)
-        myDataSource.destroy()
-    })
-
-app = express();
-
-app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(routes);
 
-app.get("/ping", (req, rse) => {
-    res.json({ message : "pong" })
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
 });
 
+const server = http.createServer(app);
 const PORT = process.env.PORT;
 
 const start = async () => {
-    try {
-        app.listen(PORT, () => {
-            console.log(`server is listening on ${PORT}`)
-        })
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 start();
