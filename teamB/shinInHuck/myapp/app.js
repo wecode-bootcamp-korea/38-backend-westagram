6438002d63d1dae5d3f351ce86a5740ff7716a5a
SHIN INHUCK <sih9492@gmail.com>
2022-10-12 01:13:12 +0900

    [ADD]게시글 등록하기

--- teamB/shinInHuck/myapp/app.js
+++ teamB/shinInHuck/myapp/app.js
@@ -1,49 +1,66 @@
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 
-const http = require('http');
+const http = require('http'); 
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
 const { DataSource } = require('typeorm');
 
-const myDataSource = new DataSource({
+const app = express();
+
+const appDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE
 })
 
-myDataSource.initialize()
+appDataSource.initialize()
     .then(() => {
-        console.log("Data Source has been initialized")
+        console.log("Data Source has been initialized!");
     })
     .catch((err) => {
-        console.error("Error during Data Source initializtion", err)
-        myDataSource.destroy()
+        console.error("Error during Data Source initialization", err);
+        appDataSource.destroy()
     })
-
-app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
-
-app.get("/ping", (req, rse) => {
-    res.json({ message : "pong" })
-});
 
 const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+    res.status(201).json({message: '!! pong !!'});
+})
+
+app.post('/posts', async (req, res) => {
+	const { title, content, user_id} = req.body
+
+	await appDataSource.query(
+		`INSERT INTO posts(
+		    title,
+		    content,
+		    user_id
+		) VALUES (?, ?, ?);
+		`,
+		[ title, content, user_id ]
+	); 
+     res.status(201).json({ "message" : "postCreated" });
+	})
+
+const server = http.createServer(app);
+
 const start = async () => {
     try {
-        app.listen(PORT, () => {
-            console.log(`server is listening on ${PORT}`)
-        })
-    } catch (err) {
-        console.error(err)
+        app.listen(PORT, ()=> console.log(`!!!!!!!!!!!server listening on port ${PORT}!!!!!!!`));
+    }
+    catch(err){
+        console.error(err);
     }
 };
 
