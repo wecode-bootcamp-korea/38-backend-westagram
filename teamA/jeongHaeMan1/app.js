982faaa22dd952c464e6f853cf185ed248425236
jeonghaeman <haemong_2@naver.com>
2022-10-11 21:06:01 +0900

    sign-up

--- teamA/jeongHaeMan1/app.js
+++ teamA/jeongHaeMan1/app.js
@@ -6,26 +6,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const routes = require('./routes');
 
-// const { DataSource } = require('typeorm');
-
-// const myDataSource = new DataSource({
-//     type: process.env.TYPEORM_CONNECTION,
-//     username: process.env.TYPEORM_USERNAME,
-//     password: process.env.TYPEORM_PASSWORD,
-//     host: process.env.TYPEORM_HOST,
-//     port: process.env.TYPEORM_PORT,
-//     database: process.env.TYPEORM_DATABASE
-// })
-
-// myDataSource.initialize()
-//     .then(() => {
-//         console.log("Data Source has been initialized!");
-//     })
-//     .catch((err) => {
-//         console.error("Error during Data Source initialization", err)
-//         myDataSource.destroy()
-//     })
-
 const app = express();
 
 app.use(express.json());
