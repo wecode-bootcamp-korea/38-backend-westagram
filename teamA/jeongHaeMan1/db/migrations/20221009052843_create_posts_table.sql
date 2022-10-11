131a8f32b2db69da5676548e99f87c81b50144ae
jeonghaeman <haemong_2@naver.com>
2022-10-11 16:22:10 +0900

    inital-setting

--- teamA/jeongHaeMan1/db/migrations/20221009052843_create_posts_table.sql
+++ teamA/jeongHaeMan1/db/migrations/20221009052843_create_posts_table.sql
@@ -1,5 +1,15 @@
 -- migrate:up
-
+CREATE TABLE posts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    content VARCHAR(30) NOT NULL,
+    image_url VARCHAR(1000) NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT posts_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 -- migrate:down
+DROP TABLE posts;
 
--- teamA/jeongHaeMan1/db/migrations/20221009052902_create_likes_table.sql
+++ teamA/jeongHaeMan1/db/migrations/20221009052902_create_likes_table.sql
@@ -1,5 +1,10 @@
 -- migrate:up
-
+CREATE TABLE likes (
+    id INT NOT NULL PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    CONSTRAINT likes_user_id_fkey, likes_post_id_fkey FOREIGN KEY (user_id, post_id) REFERENCES users(id), posts(id) ON DELETE CASCADE
+)
 
 -- migrate:down
-
+DROP TABLE likes
