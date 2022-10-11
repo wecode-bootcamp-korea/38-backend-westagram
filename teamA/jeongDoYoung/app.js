ea2e95de634b5f21fd28727125487156123c77fd
정도영 <momtop11@gmail.com>
2022-10-11 15:30:48 +0900

    Layered Pattern 제거중

--- teamA/jeongDoYoung/app.js
+++ teamA/jeongDoYoung/app.js
@@ -8,26 +8,50 @@ const cors = require("cors");
 //custom package
 const app = express();
 const routes = require("./routes");
+const routes = require("./routes");
+const { DataSource } = require('typeorm');
 
-
-app.use(cors());
-app.use(morgan('combined'));
+const myDataSource = new DataSource({
+  type: process.env.TYPOEORM_CONNECTION,
+  host: process.env.TYPOEORM_HOST,
+  port: process.env.TYPOEORM_PORT,
+  username: process.env.TYPOEORM_USERNAME,
+  password: process.env.TYPOEORM_PASSWORD,
+  database: process.env.TYPOEORM_DATABASE
+});
+myDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  }); 
 app.use(express.json());
-app.use(routes);
+app.use(cors());
+app.use(morgan('dev'));
+//[GET] 통신 heath check code
+app.get('/ping', async (req, res) => {
+  res.json({ "message" : "pong" });
+});
 
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
+//[POST] user sign up code
+app.post('/signup', async (req, res) => {
+  const { name, email, profile_image, password } = req.body;
+
+  await myDataSource.query(
+    `
+    INSERT INTO users (
+      name,
+      email,
+      profile_image,
+      password
+    ) VALUES ( ?, ?, ?, ? );
+    `,
+    [ name, email, profile_image, password ]
+  );
+  res.status(201).json({ "message" : "userCreated" });
 });
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
-
 const start = async () => {
-  try {
-    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
-  } catch (err) {
-    console.error(err);
-  }
+  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 };
-
 start();
