b86acd84a65ac24e5e7ad5709a3b9f1e7401edc1
SHIN INHUCK <sih9492@gmail.com>
2022-10-12 10:31:37 +0900

    [ADD] 전체 게시글 조회하기

--- teamB/shinInHuck/myapp/app.js
+++ teamB/shinInHuck/myapp/app.js
@@ -1,67 +1,57 @@
-const dotenv = require('dotenv').config();
-
-const http = require('http'); 
+const http = require('http');
 
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const { DataSource } = require('typeorm');
 
-const app = express();
-
 const appDataSource = new DataSource({
-    type: process.env.TYPEORM_CONNECTION,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE
-})
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
+});
 
 appDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!");
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err);
-        appDataSource.destroy()
-    })
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  });
 
-const PORT = process.env.PORT;
+const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(cors());
+app.use(morgan('tiny'));
 app.get('/ping', (req, res) => {
-    res.status(201).json({message: '!! pong !!'});
-})
+  res.json({ message : 'pong' });
+});
 
-app.post('/posts', async (req, res) => {
-	const { title, content, user_id} = req.body
-
-	await appDataSource.query(
-		`INSERT INTO posts(
-		    title,
-		    content,
-		    user_id
-		) VALUES (?, ?, ?);
-		`,
-		[ title, content, user_id ]
-	); 
-     res.status(201).json({ "message" : "postCreated" });
-	})
+app.get('/posts/get', async (req, res) => {  
+  await appDataSource.query(
+    `SELECT
+      p.user_id AS userId,
+      u.profile_image AS userProfileImage,
+      p.id AS postingId,
+      p.post_image AS postingImageUrl,
+      p.content AS postingContent
+      FROM users as u, posts as p
+      WHERE users.id = posts.user_id`
+    ,(err, rows) => {
+      res.status(200).json({ data : rows });
+    }
+  );
+});
 
 const server = http.createServer(app);
+const PORT = process.env.PORT;
 
 const start = async () => {
-    try {
-        app.listen(PORT, ()=> console.log(`!!!!!!!!!!!server listening on port ${PORT}!!!!!!!`));
-    }
-    catch(err){
-        console.error(err);
-    }
-};
-
+  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+}
 start();
