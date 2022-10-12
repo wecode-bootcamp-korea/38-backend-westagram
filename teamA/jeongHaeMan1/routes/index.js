0299155c1b5c31f6872f8687ab3c0fb3a8e8db69
jeonghaeman <haemong_2@naver.com>
2022-10-12 09:26:41 +0900

    sign-up

--- teamA/jeongHaeMan1/routes/index.js
+++ teamA/jeongHaeMan1/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require("./userRouter");
+
 router.use("/users", userRouter.router);
 
 module.exports = router;
