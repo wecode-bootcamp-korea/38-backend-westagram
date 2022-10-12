a053dc3ad94175e396fab938a1c7e0e512dcc113
Hey-Hello-World <107197044+Hey-Hello-World@users.noreply.github.com>
2022-10-12 14:08:22 +0900

    Update postController.js

--- teamB/kimSungHoon/controllers/postController.js
+++ teamB/kimSungHoon/controllers/postController.js
@@ -58,7 +58,7 @@ const deletePost = async (req, res) => {
 
 module.exports = {
 	createPost,
-    readingPost,
-    updatePost,
-    deletePost
+    	readingPost,
+    	updatePost,
+    	deletePost
 }
